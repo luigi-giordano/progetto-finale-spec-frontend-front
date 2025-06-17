b193427652b3ce4ac43d0a680cff99d95277436a
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-17 10:31:28 +0200

    [install react-router-dom][creation components/pages/hooks][setup vari]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Favorites from './pages/Favorites';
+import Compare from './pages/Compare';
 
-function App() {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/compare" element={<Compare />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
-
-export default App
